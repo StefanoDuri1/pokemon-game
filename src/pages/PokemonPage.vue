d2d11da5eb941ccf953eca7f7f447fa74ae2fcac
Stefano Duri <duristefano@gmail.com>
2023-02-01 21:23:50 -0300

    First structure

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Who is this Pokemon?</h1>
+
+        <PokemonImage/>
+        <PokemonOptions/>
+    </div>
+</template>
+<script>
+import PokemonImage from '../components/PokemonImage.vue'
+import PokemonOptions from '../components/PokemonOptions.vue'
+
+export default {
+    components: { 
+        PokemonOptions, 
+        PokemonImage 
+    },
+
+}
+</script>
