b03ce2d2a5edbf9cb6de12d193c5421d20dd8f0a
Stefano Duri <duristefano@gmail.com>
2023-02-02 12:30:49 -0300

    adding img styles

--- src/components/PokemonImage.vue
+++ src/components/PokemonImage.vue
@@ -1,13 +1,37 @@
 <template>
-    <div>
-        <h2>Pokemon Picture</h2>
+    <div class="pokemon-container">
+        <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+
+        <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+
     </div>
 </template>
+
 <script>
 export default {
     
 }
 </script>
-<style>
-    
+
+<style scoped>
+
+    .pokemon-container {
+        height: 200px;
+        display: flex;
+        justify-content: center;
+        padding: 30px 0;
+    }
+
+    img {
+        height: 200px;
+        position: absolute;
+        user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        -webkit-user-drag: none;
+        -webkit-user-select: none;
+    }
+    .hidden-pokemon {
+        filter: brightness(0%)
+    }
 </style>
